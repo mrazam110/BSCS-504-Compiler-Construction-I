6a599a4f86f573d4eb750935484285e2a8e210b6
Muhammad Raza Master <mrazam110@gmail.com>
2015-10-18 15:54:02 +0500

    generic integration syntax
    generic integration syntax

--- LexicalAnaylzerRexton/LexicalAnaylzerRexton/SyntaxAnalyzer.cs
+++ LexicalAnaylzerRexton/LexicalAnaylzerRexton/SyntaxAnalyzer.cs
@@ -13,7 +13,49 @@ namespace LexicalAnaylzerRexton
         public SyntaxAnalyzer(List<token> tokenList)
         {
             this.tokenList = tokenList;
+            addDollarToken();
+            
         }
 
+        private void addDollarToken(){
+            token temp = new token(-1, "$", "$");
+            tokenList.Add(temp);
+        }
+
+        private bool validateCfg()
+        {
+            CFG cfg = new CFG(tokenList);
+            return cfg.validate();
+        }
+
+    }
+
+    class CFG
+    {
+        private int index = 0;
+        private List<token> tokenList;
+
+        public CFG(List<token> tokenList)
+        {
+            this.tokenList = tokenList;
+        }
+
+        public bool validate()
+        {
+            if (S())
+            {
+                if (tokenList[index].classStr == "$")
+                {
+
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool S()
+        {
+            return false;
+        }
     }
 }
