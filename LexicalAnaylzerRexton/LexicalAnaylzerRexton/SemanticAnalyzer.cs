efaba657709b059a1556349a6fbc9f74e04e9446
Muhammad Raza Master <mrazam110@gmail.com>
2015-12-01 03:37:01 +0500

    data structure formed
    data structure formed

--- LexicalAnaylzerRexton/LexicalAnaylzerRexton/SemanticAnalyzer.cs
+++ LexicalAnaylzerRexton/LexicalAnaylzerRexton/SemanticAnalyzer.cs
@@ -80,7 +80,7 @@ namespace LexicalAnaylzerRexton
 
         public bool AddConstructor(CLASSMEMBER obj)
         {
-            if (obj.name == NamespaceSymbolTable.Last().classes.Last().name)
+            if (obj.name == GlobalSymbolTable.Last().classes.Last().name)
             {
                 bool RT = AddMember(obj);
                 return RT;
@@ -91,6 +91,219 @@ namespace LexicalAnaylzerRexton
                 AddMember(obj);
                 return false;
             }
+        }
+
+        public bool AddVariables(VARIABLE obj)
+        {
+            VARIABLE currentVariable = (VARIABLE)obj.ShallowCopy();
+            if (LookUpVariable(currentVariable.name, currentVariable.scope))
+            {
+                currentVariable.name = "ERROR: " + currentVariable.name;
+                GlobalSymbolTable.Last().classes.Last().members.Last().variables.Add(currentVariable);
+                return false;
+
+            }
+            else
+            {
+                GlobalSymbolTable.Last().classes.Last().members.Last().variables.Add(currentVariable);
+                return true;
+            }
+        }
+
+        public bool LookUpVariable(string name, int scope)
+        {
+            if (GlobalSymbolTable.Last().classes.Last().members.Count > 0)
+            {
+
+                for (int i = 0; i < GlobalSymbolTable.Last().classes.Last().members.Last().variables.Count; i++)
+                {
+                    if (GlobalSymbolTable.Last().classes.Last().members.Last().variables[i].scope == scope &&
+                        GlobalSymbolTable.Last().classes.Last().members.Last().variables[i].name == name)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool LookUpClass(string name)
+        {
+
+            for (int j = 0; j < GlobalSymbolTable[GlobalSymbolTable.Count - 1].classes.Count; j++)
+            {
+                if (GlobalSymbolTable.Last().classes[j].name == name) return true;
+            }
+
+            return false;
+
+        }
+        public bool LookUpGlobal(string name)
+        {
+            for (int i = 0; i < GlobalSymbolTable.Count; i++)
+            {
+                if (GlobalSymbolTable[i].name == name) return true;
+            }
+            return false;
+        }
+
+        public bool LookUpMember(CLASSMEMBER obj)
+        {
+            bool paramNotMatch = true;
+            if (obj.isMethod)
+            {
+                for (int i = 0; i < GlobalSymbolTable.Last().classes.Last().members.Count; i++)
+                {
+                    if (GlobalSymbolTable.Last().classes.Last().members[i].isMethod &&
+                        GlobalSymbolTable.Last().classes.Last().members[i].name == obj.name)
+                    {
+                        if (GlobalSymbolTable.Last().classes.Last().members[i].param.Count != obj.param.Count)
+                        {
+                            continue; // To next class member, count not same
+                        }
+                        string objParam = "";
+                        string memberParam = "";
+
+                        foreach (string s in GlobalSymbolTable.Last().classes.Last().members[i].param)
+                        {
+                            memberParam += s;
+                        }
+
+                        foreach (string s in obj.param)
+                        {
+                            objParam += s;
+                        }
+
+                        if (objParam != memberParam)
+                        {
+                            paramNotMatch = true;
+                        }
+                        else
+                        {
+                            paramNotMatch = false;
+                            break;
+                        }
+
+                        // Param traverse
+                        //for (int j = 0; j < GlobalSymbolTable.Last().classes.Last().members[i].param.Count; j++)
+                        //{
+                        //    if (GlobalSymbolTable.Last().classes.Last().members[i].param[j] != obj.param[j])
+                        //    {
+                        //        paramNotMatch = true;
+                        //        break;
+                        //    }
+                        //}
+
+                    }
+                }
+
+                if (paramNotMatch)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            else
+            {
+                for (int i = 0; i < GlobalSymbolTable.Last().classes.Last().members.Count; i++)
+                {
+                    if (!GlobalSymbolTable.Last().classes.Last().members[i].isMethod &&
+                        GlobalSymbolTable.Last().classes.Last().members[i].name == obj.name)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool LookUpConstructor(CLASSMEMBER obj)
+        {
+            if (obj.param.Count == 0)
+            {
+                return true;
+            }
+            string obj_s = ""; // object params in one string
+            string mem_s = ""; // class member params in one string
+            foreach (string s in obj.param)
+            {
+                obj_s += s;
+            }
+            for (int j = 0; j < GlobalSymbolTable[GlobalSymbolTable.Count - 1].classes.Count; j++)
+            {
+                if (GlobalSymbolTable.Last().classes[j].name == obj.type)
+                {
+                    foreach (CLASSMEMBER i in GlobalSymbolTable.Last().classes[j].members)
+                    {
+                        if (obj.type == i.name)
+                        {
+                            foreach (string s in i.param)
+                            {
+                                mem_s += s;
+                            }
+                            if (mem_s == obj_s)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                mem_s = "";
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public bool LookUpLabel(string name)
+        {
+            int currScope = this.CurrentScope();
+            for (int i = 0; i < GlobalSymbolTable.Last().classes.Last().members.Last().variables.Count; i++)
+            {
+                if (GlobalSymbolTable.Last().classes.Last().members.Last().variables[i].scope == currScope &&
+                    GlobalSymbolTable.Last().classes.Last().members.Last().variables[i].type == "Label" &&
+                    GlobalSymbolTable.Last().classes.Last().members.Last().variables[i].name == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string removeIndex(string actualType, string toRemove)
+        {
+
+            string newType = "";
+            int index = actualType.IndexOf(toRemove);
+            if (index != -1)
+            {
+                newType = actualType.Substring(0, index);
+                return newType;
+            }
+            return "invalid";
+
+        }
+
+        public string getCurrentClass()
+        {
+            return GlobalSymbolTable.Last().classes.Last().name;
+        }
+
+        public void reset()
+        {
+            Scope.Clear();
+            scopeToAdd = 0;
+            GlobalSymbolTable.Clear();
         }
     }
 }
